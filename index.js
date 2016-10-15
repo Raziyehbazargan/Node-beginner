44dfccb7d7345aa68192676783a077006050cf28
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-15 15:21:03 -0700

    Handling POST requests

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var requestHandlers = require("./requestHandlers.js");
 var handle = {};
 
 handle["/"] = requestHandlers.start;
-handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 
 server.start(router.route, handle);
