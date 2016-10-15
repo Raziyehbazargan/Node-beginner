9ba4b0068cd3bbce60bfb2cdcb686246ef57302e
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-15 15:38:28 -0700

    POST - send text

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring"); 
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
 exec("ls -lah", function (error, stdout, stderr) { 
@@ -24,10 +25,11 @@ exec("ls -lah", function (error, stdout, stderr) {
   });
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"}); 
-  response.write("Hello Upload");
+  response.write("You've sent the text: " +
+  querystring.parse(postData).text);  
   response.end();
 }
 
--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const http = require('http');
 const url = require('url');
 
 function start(route, handle) {
-    function onRequest(req,response) {
-        var pathname = url.parse(req.url).pathname;
+    function onRequest(request,response) {
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.")
         
-        route(handle , pathname, response);
+        request.setEncoding("utf8");
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+ postDataChunk + "'.");
+        });
+        request.addListener("end", function() { 
+            route(handle, pathname, response, postData);
+        });
     };
     http.createServer(onRequest).listen(8888)
     console.log('server is up on 8888');
